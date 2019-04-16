3154e8ca337e4f776b6d1d0a2a428c2968434142
Bryan Santos <49533832+BryanSantoos@users.noreply.github.com>
2019-04-16 19:23:33 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.grpAritmetica = new System.Windows.Forms.GroupBox();
+            this.rdbSomar = new System.Windows.Forms.RadioButton();
+            this.rdbSubtrair = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicar = new System.Windows.Forms.RadioButton();
+            this.rdbDividir = new System.Windows.Forms.RadioButton();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.grpAritmetica.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(60, 23);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(14, 13);
+            this.lblX.TabIndex = 0;
+            this.lblX.Text = "X";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(60, 49);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(14, 13);
+            this.lblY.TabIndex = 1;
+            this.lblY.Text = "Y";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(32, 75);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(42, 13);
+            this.lblTotal.TabIndex = 2;
+            this.lblTotal.Text = "TOTAL";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(80, 19);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(84, 20);
+            this.txtX.TabIndex = 3;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(80, 46);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(84, 20);
+            this.txtY.TabIndex = 4;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(80, 72);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(84, 20);
+            this.txtTotal.TabIndex = 5;
+            // 
+            // grpAritmetica
+            // 
+            this.grpAritmetica.Controls.Add(this.rdbDividir);
+            this.grpAritmetica.Controls.Add(this.rdbMultiplicar);
+            this.grpAritmetica.Controls.Add(this.rdbSubtrair);
+            this.grpAritmetica.Controls.Add(this.rdbSomar);
+            this.grpAritmetica.Location = new System.Drawing.Point(35, 108);
+            this.grpAritmetica.Name = "grpAritmetica";
+            this.grpAritmetica.Size = new System.Drawing.Size(129, 120);
+            this.grpAritmetica.TabIndex = 6;
+            this.grpAritmetica.TabStop = false;
+            this.grpAritmetica.Text = "OPERAÇÕES";
+            // 
+            // rdbSomar
+            // 
+            this.rdbSomar.AutoSize = true;
+            this.rdbSomar.Location = new System.Drawing.Point(7, 20);
+            this.rdbSomar.Name = "rdbSomar";
+            this.rdbSomar.Size = new System.Drawing.Size(64, 17);
+            this.rdbSomar.TabIndex = 0;
+            this.rdbSomar.Text = "SOMAR";
+            this.rdbSomar.UseVisualStyleBackColor = true;
+            // 
+            // rdbSubtrair
+            // 
+            this.rdbSubtrair.AutoSize = true;
+            this.rdbSubtrair.Location = new System.Drawing.Point(7, 43);
+            this.rdbSubtrair.Name = "rdbSubtrair";
+            this.rdbSubtrair.Size = new System.Drawing.Size(80, 17);
+            this.rdbSubtrair.TabIndex = 1;
+            this.rdbSubtrair.Text = "SUBTRAIR";
+            this.rdbSubtrair.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicar
+            // 
+            this.rdbMultiplicar.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.rdbMultiplicar.AutoSize = true;
+            this.rdbMultiplicar.Location = new System.Drawing.Point(7, 67);
+            this.rdbMultiplicar.Name = "rdbMultiplicar";
+            this.rdbMultiplicar.Size = new System.Drawing.Size(96, 17);
+            this.rdbMultiplicar.TabIndex = 2;
+            this.rdbMultiplicar.Text = "MULTIPLICAR";
+            this.rdbMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // rdbDividir
+            // 
+            this.rdbDividir.AutoSize = true;
+            this.rdbDividir.Checked = true;
+            this.rdbDividir.Location = new System.Drawing.Point(7, 91);
+            this.rdbDividir.Name = "rdbDividir";
+            this.rdbDividir.Size = new System.Drawing.Size(65, 17);
+            this.rdbDividir.TabIndex = 3;
+            this.rdbDividir.TabStop = true;
+            this.rdbDividir.Text = "DIVIDIR";
+            this.rdbDividir.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(35, 234);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(59, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.SystemColors.Control;
+            this.btnFechar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(100, 234);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(64, 23);
+            this.btnFechar.TabIndex = 9;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(35, 263);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(129, 26);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 311);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.grpAritmetica);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmCalculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.frmCalculadora_Load);
+            this.grpAritmetica.ResumeLayout(false);
+            this.grpAritmetica.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.GroupBox grpAritmetica;
+        private System.Windows.Forms.RadioButton rdbDividir;
+        private System.Windows.Forms.RadioButton rdbMultiplicar;
+        private System.Windows.Forms.RadioButton rdbSubtrair;
+        private System.Windows.Forms.RadioButton rdbSomar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
